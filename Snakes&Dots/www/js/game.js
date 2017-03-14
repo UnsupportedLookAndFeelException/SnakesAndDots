0b8deecb16a766e15a7adf62ae097eac61531398
UnsupportedLookAndFeelException <calebhwin@gmail.com>
2017-03-14 13:34:56 -0700

    Update game.js

--- Snakes&Dots/www/js/game.js
+++ Snakes&Dots/www/js/game.js
@@ -16,11 +16,10 @@ canvas.addEventListener("mouseup", function(e) {
     isMouseClicked = true;
 }, false);*/
 canvas.addEventListener("touchstart", function(e) {
+    mousePos = getTouchPos(canvas, e);
     isMousePressed = true;
 }, false);
 canvas.addEventListener("touchend", function(e) {
-    mousePos = getTouchPos(canvas, e);
-    alert(mousePos.x+","+mousePos.y);
     isMousePressed = false;
     isMouseClicked = true;
 }, false);
